612a340782d54d750c73df1179ce4d351397f024
Markus Konrad <post@mkonrad.net>
2019-12-23 20:26:50 +0100

    extended euclidian algo

--- gcd/gcd.jl
+++ gcd/gcd.jl
@@ -1,6 +1,6 @@
 module GCD
 
-export gcd_euclid
+export gcd_euclid, gcd_euclid_extended
 
 function gcd_euclid(a::Integer, b::Integer)
     a == 0 && b == 0 && return 0
@@ -15,6 +15,46 @@ function gcd_euclid(a::Integer, b::Integer)
         r == 0 && return a
         b = a
         a = r
+    end
+end
+
+function gcd_euclid_extended(a::Integer, b::Integer)
+    a == 0 && b == 0 && return 0, 0, 0
+
+
+    if a == 0    # swap
+        a, b = b, a
+    end
+
+    qs = Vector{promote_type(typeof(a), typeof(b))}()
+    i = 0
+
+    while true
+        q = fld(b, a)
+        r = mod(b, a)
+
+        if r < 0
+            r -= a
+            q += 1
+        end
+
+        @assert 0 <= r < abs(a)
+        @assert b == q * a + r
+
+        if r == 0
+            s, t = 0, 1
+            for j in i:-1:1
+                s, t = t, s - qs[j] * t
+            end
+
+            return a, t, s
+        end
+
+        push!(qs, q)
+
+        b = a
+        a = r
+        i += 1
     end
 end
 
--- gcd/tests.jl
+++ gcd/tests.jl
@@ -2,7 +2,7 @@ using Test: @test, @testset
 
 include("gcd.jl")
 
-using .GCD: gcd_euclid
+using .GCD
 
 @testset "gcd_euclid" begin
     @test gcd_euclid(0, 0) == 0
@@ -13,5 +13,24 @@ using .GCD: gcd_euclid
     @testset "random inputs against Julia gcd impl." for i in 1:100
         a, b = rand(Int), rand(Int)
         @test gcd_euclid(a, b) == gcd(a, b)
+    end
+end
+
+
+@testset "gcd_euclid_extended" begin
+    @test gcd_euclid_extended(0, 0) == (0, 0, 0)
+    @test gcd_euclid_extended(1, 1) == (1, 1, 0)
+
+    d, s, t = gcd_euclid_extended(-2002, 210)
+    @test gcd_euclid_extended(210, -2002) == (d, t, s) == (14, -19, -2)
+
+    d, s, t = gcd_euclid_extended(128, 34)
+    @test gcd_euclid_extended(34, 128) == (d, t, s) == (2, -15, 4)
+
+    @testset "random inputs against Julia gcd impl." for i in 1:100
+        a, b = rand(Int), rand(Int)
+        d, s, t = gcd_euclid_extended(a, b)
+        @test d == gcd(a, b)
+        @test d == a*s + b*t
     end
 end
