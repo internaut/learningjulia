1e1247a3769b6990fc9f654d47a9d73b1efbd6ff
Markus Konrad <post@mkonrad.net>
2019-12-28 14:40:18 +0100

    residue class: inverse and comp. operators (WIP)

--- residueclasses/rclass.jl
+++ residueclasses/rclass.jl
@@ -1,6 +1,8 @@
 module ResidueClasses
 
-import Base: +, -, *
+export RClass, +, -, *, /, inv, hasinv, ^, ==, <=, <, >=, >
+
+import Base: +, -, *, /, inv, ^, ==, <=, <, >=, >
 
 """
 Residue class ā in ring ℤ modulo Nℤ
@@ -13,8 +15,8 @@ struct RClass{N}
     end
 
     function RClass{N}(a::Integer) where {N}
-        if N < 2
-            error("N must be > 1")
+        if !(typeof(N) <: Integer) || N < 2
+            error("N must be integer > 1")
         end
 
         new(mod(a, N))
@@ -30,16 +32,36 @@ Base.show(io::IO, r::RClass{N}) where {N} = print(io, r.a, " in ℤ/", N, "ℤ")
 +(x::RClass{N}) where {N} = RClass{N}(x.a)
 -(x::RClass{N}) where {N} = RClass{N}(-Int(x.a))
 
-# TODO: inv() and helper hasinv()
+"Returns `true` when residual class `x` has inverse, otherwise `false`."
+hasinv(x::RClass{N}) where {N} = gcd(x.a, N) == 1
+
+function inv(x::RClass{N})::RClass{N} where {N}
+    @assert 0 <= x.a < N
+
+    d, s, t = gcdx(Int(x.a), N)
+    d == 1 || error("residual class ", x, " has no inverse")
+
+    modinv = s - fld(s, N) * N   # normalize
+    @assert 0 <= modinv < N
+
+    RClass{N}(modinv)
+end
 
 # binary arithmetic operators
 
 +(x::RClass{N}, y::RClass{N}) where {N} = RClass{N}(x.a + y.a)
 -(x::RClass{N}, y::RClass{N}) where {N} = RClass{N}(x.a + (-y).a)
 *(x::RClass{N}, y::RClass{N}) where {N} = RClass{N}(x.a * y.a)
+/(x::RClass{N}, y::RClass{N}) where {N} = RClass{N}((x * inv(y)).a)
+# TODO: fix test for this:
+^(x::RClass{N}, y::Integer) where {N} = y >= 0 ? RClass{N}(x.a ^ y) : inv(RClass{N}(x.a ^ abs(y)))
 
-# TODO: / as x*inv(y)
-
-# TODO binary comparison operators
+# comparison operators
+# TODO: implement tests for these
+==(x::RClass{N}, y::RClass{N}) where {N} = x.a == y.a
+<=(x::RClass{N}, y::RClass{N}) where {N} = x.a <= y.a
+<(x::RClass{N}, y::RClass{N}) where {N} = x.a < y.a
+>=(x::RClass{N}, y::RClass{N}) where {N} = x.a >= y.a
+>(x::RClass{N}, y::RClass{N}) where {N} = x.a > y.a
 
 end
--- residueclasses/tests.jl
+++ residueclasses/tests.jl
@@ -2,7 +2,7 @@ using Test: @test, @testset, @test_throws
 
 include("rclass.jl")
 
-using .ResidueClasses: RClass, +, -, *
+using .ResidueClasses
 
 @testset "RClass" begin
     for (x, expected) in zip(-3:3, Iterators.cycle([1, 0]))
@@ -27,6 +27,7 @@ end
     @test z.a == (+z).a
     @test (-z).a == 1
     @test (-(-z)).a == z.a == 2
+    @test inv(z).a == 2
 
     @testset "random inputs for unary arithmetic operators" for i = 1:100
         a = rand(1:1000)
@@ -35,6 +36,11 @@ end
 
         # comparing z.a here to make it indep. of comparison operator == implementation
         @test (+z).a == (-(-z)).a == z.a
+        if hasinv(z)
+            @test inv(inv(z)).a == z.a
+        else
+            @test_throws ErrorException inv(z)
+        end
     end
 end
 
@@ -43,6 +49,7 @@ end
     @test_throws MethodError RClass{2}(0) + RClass{3}(0)
     @test_throws MethodError RClass{2}(0) - RClass{3}(0)
     @test_throws MethodError RClass{2}(0) * RClass{3}(0)
+    @test_throws MethodError RClass{2}(0) / RClass{3}(0)
 
     x = RClass{3}(2)
     y = RClass{3}(1)
@@ -50,6 +57,12 @@ end
     @test (x + y).a == (y + x).a == 0
     @test (x - y).a == (-(y - x)).a == 1
     @test (x * y).a == (y * x).a == 2
+    @test (x / y).a == (x * inv(y)).a == inv(y / x).a == 2
+    @test (x^-2).a == inv(x * x).a == 1
+    @test (x^-1).a == inv(x).a == 2
+    @test (x^0).a == 1
+    @test (x^1).a == x.a == 2
+    @test (x^2).a == (x * x).a == 1
 
     @testset "random inputs for binary arithmetic operators" for i = 1:100
         N = rand(2:100)
@@ -59,5 +72,48 @@ end
         @test 0 <= (x + y).a == (y + x).a < N
         @test 0 <= (x - y).a == (-(y - x)).a < N
         @test 0 <= (x * y).a == (y * x).a < N
+
+        if hasinv(x)
+            x_inv = inv(x)
+            @test (x * x_inv).a == 1
+        else
+            x_inv = missing
+            @test_throws ErrorException inv(x)
+        end
+
+        if hasinv(y)
+            y_inv = inv(y)
+            @test (y * y_inv).a == 1
+            @test 0 <= (x / y).a == (x * y_inv).a < N
+        else
+            y_inv = missing
+            @test_throws ErrorException inv(x)
+        end
+
+        if !(ismissing(x_inv) || ismissing(y_inv))
+            @test (x / y).a == inv((y / x)).a
+        end
+
+        # TODO: fix this test
+        # for p = -5:5
+        #     if p == 0
+        #         z = RClass{N}(1)
+        #     else
+        #         z = RClass{N}(x.a)
+        #         for j = 1:abs(p)
+        #             z *= z
+        #         end
+        #     end
+        #
+        #     if p < 0
+        #         if hasinv(x)
+        #             @test (x^p).a == inv(z).a
+        #         else
+        #             @test_throws ErrorException x^p
+        #         end
+        #     else
+        #         @test (x^p).a == z.a
+        #     end
+        # end
     end
 end
